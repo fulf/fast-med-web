dd873747b07397cc81d7883c411850b45ba5270e
Sorin Guga <sorin.guga@e-spres-oh.com>
2016-05-11 21:38:10 +0300

    Refactored Database Connection Wrapper.

--- src/php/conn.php
+++ src/php/conn.php
@@ -1,12 +1,20 @@
 <?php
+	function db_query($query)
+	{
+		$conn = mysqli_connect("db.fastmed.soringuga.ro", "fastmed", "fastmed123", "fastmed_db");
 
-	$conn = mysqli_connect("db.fastmed.soringuga.ro", "fastmed", "fastmed123", "fastmed_db");
+		if (!$conn) {
+		    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+		    exit;
+		}
 
-	if (!$conn) {
-	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-	    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-	    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-	    exit;
+		if (!$rs = mysqli_query($conn, $query))
+		{
+			echo "<b>Error:</b> ".mysqli_error($conn)."<br/><br/>";
+			return false;
+		}
+		return $rs;
 	}
-
 ?>
