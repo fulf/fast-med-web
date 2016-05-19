5697bfbac06ad71e5c666e30c3fd24ed0938b3f8
Sorin Guga <sorin.guga@e-spres-oh.com>
2016-05-19 11:47:23 +0300

    Added frontend login functionality.

--- src/js/login.js
+++ src/js/login.js
@@ -1,1 +1,25 @@
-angular.module('FastMed', ['ngMaterial']);
+angular.module('FastMed', ['ngMaterial', 'ngMessages'])
+    .controller("loginController", function($http, $scope){
+        $scope.state = null;
+        $scope.login = function(){
+            $scope.state = 'loading';
+            $http({
+                method: 'POST',
+                url: 'api/authenticate.php',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: { 
+                    'username': $scope.user.username,
+                    'password': $scope.user.password
+                }
+            }).then(
+                function(){
+                    $scope.state = 'success';
+                },
+                function(){
+                    $scope.state = 'error';
+                }
+            );
+        }
+    });
