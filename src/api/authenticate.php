ad93c7df545421aef8b44264185f892d30289fcc
Sorin Guga <sorin.guga@e-spres-oh.com>
2016-05-19 11:45:33 +0300

    Created authentication API

---
+++ src/api/authenticate.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == 'DELETE')
+	require 'authenticate/DELETE.php';
+else if($_SERVER['REQUEST_METHOD'] == 'POST')
+	require 'authenticate/POST.php';
+
+function gracefulExit($status, $success, $data)
+{
+	header('Content-Type: application/json');
+	http_response_code($status);
+	exit(json_encode(array(
+		"success"=>$success,
+		"data"=>$data
+	)));
+}
+?>
