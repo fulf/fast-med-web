8c142a16fe4c103d3b35e18b0d4b8d44918b51e1
Sorin Guga <sorin.guga@e-spres-oh.com>
2016-06-01 20:57:21 +0300

    Added robots endpoint to API

--- src/api/fixtures/robotsFixture.php
+++ src/api/fixtures/robotsFixture.php
@@ -1,6 +1,9 @@
 <?php
 require_once "conn.php";
 
+$robotsJSON = file_get_contents('../assets/fixtures/robots.json');
+$robotFixture = json_decode($robotsJSON, true);
+
 echo "<i>Dropping</i> existing Robots table.<br/>";
 if(db_query("DROP TABLE IF EXISTS Robots")) echo "<b>Success!</b><br/><br/>";
 
@@ -12,5 +15,17 @@ $query = "CREATE TABLE `Robots` (
         ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;";
 
 if(db_query($query)) echo "<b>Success!</b><br/><br/>";
+
+foreach ($robotFixture as $id => $robot) {
+    echo "<i>Inserting</i> drug ".($id+1).".<br/>";
+
+    $query = "INSERT INTO 
+			Robots(
+				Name
+			) VALUES(
+				'".$robot["Name"]."'
+			);";
+    if(db_query($query)) echo "<b>Success!</b><br/><br/>";
+}
 
 echo "Done.<br/>";
---
+++ src/api/robots.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+if($_SESSION['login'] != 'ok')
+    gracefulExit(401, false, "Unauthorised request. This incident has been reported.");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET')
+    require 'robots/GET.php';
+
+function gracefulExit($status, $success, $data)
+{
+    header('Content-Type: application/json');
+    http_response_code($status);
+    exit(json_encode(array(
+        "success"=>$success,
+        "data"=>$data
+    )));
+}
